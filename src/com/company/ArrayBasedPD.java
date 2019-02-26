f16b6929086c82f632fe6615494125824b32e6bd
Evan Barry <Evan.Barry@students.ittralee.ie>
2019-02-26 10:58:46 +0000

    Initial commit

---
+++ src/com/company/ArrayBasedPD.java
@@ -0,0 +1,122 @@
+package com.company;
+
+import javax.swing.*;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayBasedPD implements PhoneDirectory {
+    // phone directory and related items
+    private final int MAXENTRIES = 200; // maximum number of entries in phone directory
+    private PhoneDirectoryEntry[] phoneDirectory = new PhoneDirectoryEntry[MAXENTRIES];
+    private int numberOfEntries = 0; // number of entries
+    private int currentEntry = -1; //index of entry currently displayed
+
+    public void loadData(String filename) {
+        // TODO Auto-generated method stub
+        try {
+            File inputFile = new File(filename);
+            Scanner in = new Scanner(inputFile);
+            String name;
+            String number;
+            int index = 0;
+
+            while (in.hasNext()){
+                name = in.next();
+                number = in.next();
+                phoneDirectory[index] = new PhoneDirectoryEntry(name, number);
+                index++;
+            }
+
+            numberOfEntries = index;
+            //now call Arrays.sort() to sort phoneDirectory
+            Arrays.sort(phoneDirectory,0,numberOfEntries);
+
+            in.close();
+
+        } catch (IOException exc) {
+            System.out.println("File does not exist");
+        }
+    }
+
+    public void saveData(String filename) {
+        // TODO Auto-generated method stub
+        try {
+            FileWriter fileWriter = new FileWriter(filename);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            for(int i = 0; i < numberOfEntries; i++)
+            {
+                printWriter.println(phoneDirectory[i].getName());
+                printWriter.println(phoneDirectory[i].getNumber());
+            }
+            printWriter.close();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+    }
+
+    public void addEntry(String name, String number) {
+        // TODO Auto-generated method stub
+
+        phoneDirectory[numberOfEntries] = new PhoneDirectoryEntry(name, number);
+        numberOfEntries++;
+        Arrays.sort(phoneDirectory,0,numberOfEntries);
+    }
+
+    public PhoneDirectoryEntry getFirst() {
+        // TODO Auto-generated method stub
+        if (numberOfEntries != 0) {
+            currentEntry = 0;
+            return phoneDirectory[0];
+        } else
+            return null;
+    }
+
+    public PhoneDirectoryEntry getNext() {
+        // TODO Auto-generated method stub
+        if (currentEntry != numberOfEntries - 1) {
+            currentEntry++;
+            return phoneDirectory[currentEntry];
+        } else
+            return null;
+
+    }
+
+    public PhoneDirectoryEntry getPrevious() {
+        // TODO Auto-generated method stub
+
+        if(currentEntry != 0)
+        {
+            currentEntry--;
+            return phoneDirectory[currentEntry];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public String search(String name) {
+        // TODO Auto-generated method stub
+
+        int index = Arrays.binarySearch(phoneDirectory, 0, numberOfEntries, new PhoneDirectoryEntry(name, null));
+
+        if(index < 0)
+        {
+            //JOptionPane.showMessageDialog(null, "Entry Not Found");
+            return "Entry Not Found";
+        }
+
+        else
+        {
+            //JOptionPane.showMessageDialog(null, phoneDirectory[index].getNumber());
+            return name + "'s number is " + phoneDirectory[index].getNumber();
+        }
+    }
+}
